279c4bca0f97a3b97979eeebd7c353fa6bbb0f7a
One <appdevoney@gmail.com>
2014-10-10 20:55:46 +0800

    Initial Commit

---
+++ MemoryIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemoryIt
+//
+//  Created by SpoonRocket on 2014/10/10.
+//  Copyright (c) 2014年 one. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
